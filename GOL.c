64cd179161b1e09b7157078d82dfbd17cf9fd4cc
chilliegator <149959312+chilliegator@users.noreply.github.com>
2023-11-09 14:21:37 +0300

    Update GOL.c

--- GOL.c
+++ GOL.c
@@ -1,24 +1,21 @@
-#include <stdio.h>
 #include <time.h>
+#include <conio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <conio.h>
 
 #define SIZE (30 + 2)
 #define XY_TO_I(x, y) y *SIZE + x
-#define alive '@'
+#define alive '*'
 #define dead ' '
 #define ext_size SIZE *SIZE + 2 * SIZE
 
 // random spawn of cells in 1st generation ( рандомное появление клеток в 1ом поколении)
 void spawncells(char *grid)
 {
-    int i = 0;
-    for (; i < SIZE * SIZE; ++i)
-    {
+    for (int i = 0; i < SIZE * SIZE; ++i)
         grid[i] = (rand() % 2) ? alive : dead;
-    }
 }
 
 // draw a walls( нарисуем стены )
@@ -30,11 +27,13 @@ void drawborders(char *grid)
         grid[i] = '-';
         grid[XY_TO_I(i, (SIZE - 1))] = '-';
     }
+	
     for (i = 1; i < SIZE - 1; ++i)
     {
         grid[XY_TO_I(0, i)] = '|';
         grid[XY_TO_I((SIZE - 1), i)] = '|';
     }
+	
     grid[XY_TO_I(0, 0)] = '+';
     grid[XY_TO_I((SIZE - 1), 0)] = '+';
     grid[XY_TO_I((SIZE - 1), (SIZE - 1))] = '+';
@@ -62,15 +61,11 @@ int countNeighbors(char *grid, int x, int y)
 
             // Skip current cell ( пропускаем текущую ячейку )
             if (i == 0 && j == 0)
-            {
                 continue;
-            }
 
             // Check if neighbor is within grid boundaries ( проверяем наличие соседей в окрестностях )
             if (neighborX > 0 && neighborY > 0 && neighborX < SIZE - 1 && neighborY < SIZE - 1)
-            {
                 count += grid[XY_TO_I(neighborX, neighborY)] == alive;
-            }
         }
     }
     return count;
@@ -84,9 +79,8 @@ void printGrid(char *grid)
     for (; i < SIZE; ++i)
     {
         for (j = 0; j < SIZE; ++j)
-        {
             printf("%c", grid[XY_TO_I(j, i)]);
-        }
+		
         printf("\n");
     }
 }
@@ -105,27 +99,20 @@ void updateGrid(char *grid)
             int neighbors = countNeighbors(grid, i, j);
 
             // Apply game of life rules ( опишем правила игры )
-            if (grid[XY_TO_I(i, j)])
+            if (grid[XY_TO_I(i, j)] == alive)
             {
                 if (neighbors < 2 || neighbors > 3)
-                {
                     nextGrid[XY_TO_I(i, j)] = dead;
-                }
                 else
-                {
                     nextGrid[XY_TO_I(i, j)] = alive;
-                }
             }
             else
             {
                 if (neighbors == 3)
-                {
                     nextGrid[XY_TO_I(i, j)] = alive;
-                }
+				
                 else
-                {
                     nextGrid[XY_TO_I(i, j)] = dead;
-                }
             }
         }
     }
@@ -135,10 +122,9 @@ void updateGrid(char *grid)
 }
 
 // Clear screen function( функция очистки экрана )
-int clear_screen()
+void clear_screen()
 {
     system("cls");
-    return 0;
 }
 
 int main()
@@ -146,21 +132,23 @@ int main()
     srand(time(NULL));
     char grid[ext_size];
     char prev_grid[ext_size];
+	memcpy(prev_grid, grid, ext_size);
     memset(grid, 0, ext_size);
     // Initialize grid randomly ( задаем поле рандомно )
     initGrid(grid);
+	
     // Run game of life iterations ( запуск итераций игры )
     while (1)
     {
+		clear_screen();
         printGrid(grid);
-        Sleep(10);
-        clear_screen();
-        memcpy(prev_grid, grid, ext_size);
         updateGrid(grid);
+		// The game lasts until the current generation coincides with the previous one.
+		// Игра длится до тех пор, пока нынешнее поколение не совпадет с предыдущим.
         if (!memcmp(grid, prev_grid, ext_size))
             break;
+		
+		memcpy(prev_grid, grid, ext_size);
     }
     return 0;
 }
-// The game lasts until the current generation coincides with the previous one.
-// Игра длится до тех пор, пока нынешнее поколение не совпадет с предыдущим.
